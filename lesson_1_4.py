83444df65eb032a6e2acb6484db32e32b0f9331e
gl00mi <zlatakr132@yandex.ru>
2023-06-14 16:41:01 +0300

    initial commit

---
+++ lesson_1_4.py
@@ -0,0 +1,25 @@
+'''def strcounter(string): сложность О(n ** 2)
+    for symbol in string:
+        counter = 0
+        for sub_symbol in string:
+            if symbol == sub_symbol:
+                counter += 1
+        print(symbol, counter)
+strcounter('aaaabbcdee')
+'''
+'''
+def strcounter(string): # сложность O(n * m)
+    for symbol in set(string):
+        counter = 0
+        for sub_symbol in string:
+            if symbol == sub_symbol:
+                counter += 1
+        print(symbol, counter)
+strcounter('aaaabbcdee')
+'''
+def strcounter(string):
+    syms_counter = {}
+    for symbol in string:
+        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+    print(syms_counter)
+strcounter('aaaabbcdee')
