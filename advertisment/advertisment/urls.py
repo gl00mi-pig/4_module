a112811fca0d00afd825e0fc1661e54b37a8c972
Злата <136626503+gl00mi-pig@users.noreply.github.com>
2023-07-05 21:04:59 +0300

    lesson 4

---
+++ advertisment/advertisment/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app_advertisment.urls')),
+    path('', include('app_lesson_4.urls'))
+]
